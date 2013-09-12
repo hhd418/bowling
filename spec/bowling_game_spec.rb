b57a57d5040ee39fd6d45375ee795e5c85e331c9
MakerSquare Student <students@themakersquare.com>
2013-09-12 09:48:37 -0500

    Adds Test to make sure that when a gutter ball is rolled the score is 0."

--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -11,4 +11,12 @@ describe BowlingGame do
       expect(game.rolls).to eq([])
     end
   end
+  describe '#score' do
+    it ' scores the game as 0 when only gitter balls are thrown' do
+      game = BowlingGame.new 
+      20.times { game.roll(0)}
+
+      expect(game.score).to eq(0)
+    end
+  end
 end
