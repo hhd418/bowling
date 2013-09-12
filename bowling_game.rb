d1ab7d73d6a874ac453bfcf2aaaa189accdda0dc
MakerSquare Student <students@themakersquare.com>
2013-09-12 11:39:35 -0500

    Refactored the code to create private methods for strike and spare and other.

--- bowling_game.rb
+++ bowling_game.rb
@@ -4,9 +4,11 @@ class BowlingGame
 
   def initialize
     @rolls = []
+    @total_score = 0
+    @current_roll = 0
   end
 
-  #  Record a roll in the game.
+  # Record a roll in the game.
   #
   # pins - The Integer number of pins knocked down in this roll.
   #
@@ -14,28 +16,53 @@ class BowlingGame
   def roll(pins)
     @rolls << pins
   end
-  
+
+  # Score the game
+  #
+  # Uses private methods to account for strike, spare and regular scores
   def score
-    total_score = 0
-    current_roll = 0
+    while @current_roll < @rolls.size - 1
+      init_roll
 
-    while current_roll < @rolls.size - 1
-      roll      = @rolls[current_roll]
-      next_roll = @rolls[current_roll + 1]
-
-      if roll == 10
-        total_score += 10 + @rolls[current_roll + 1] + @rolls[current_roll + 2]
-        current_roll += 1
-      elsif roll + next_roll == 10
-        total_score += 10 + @rolls[current_roll + 2]
-        current_roll += 2
+      if strike?
+        score_strike
+      elsif spare?
+        score_spare
       else
-        total_score += roll + next_roll
-        current_roll += 2
+        regular_score
       end
     end
 
-    return total_score 
+    return @total_score 
   end
 
+  private
+
+  def init_roll
+    @roll      = @rolls[@current_roll]
+    @next_roll = @rolls[@current_roll + 1]
+  end
+
+  def strike?
+    @roll == 10
+  end
+
+  def score_strike
+    @total_score += 10 + @rolls[@current_roll + 1] + @rolls[@current_roll + 2]
+    @current_roll += 1  
+  end
+
+  def spare?
+    @roll + @next_roll == 10
+  end
+
+  def score_spare
+    @total_score += 10 + @rolls[@current_roll + 2]
+    @current_roll += 2
+  end
+
+  def regular_score
+    @total_score += @roll + @next_roll
+    @current_roll += 2
+  end
 end
